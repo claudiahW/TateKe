c14389849aaa75b00fba972b68b04d593b389788
claudiahW <murungiclaud@gmail.com>
2021-11-26 23:54:28 +0300

    create index template

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
-from . import views
+from . import views 
+
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
+    url('', views.index, name='index'),
 ]
