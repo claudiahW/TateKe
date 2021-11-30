2b287ba512e157602a4d244bf0cf39f9a8358bd1
claudiahW <murungiclaud@gmail.com>
2021-11-30 18:39:34 +0300

    add category class

--- gallery/models.py
+++ gallery/models.py
@@ -28,3 +28,11 @@ class Location(models.Model):
     def save_location(self):
         self.save()
    
+class Category(models.Model):
+    name = models.CharField(max_length = 60)
+
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()   
