e00025627a51637ac8543a3e90efa208ec2c80a9
claudiahW <murungiclaud@gmail.com>
2021-12-01 19:27:49 +0300

    edit navbar

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import photos
+from .models import photos,Category,Location
 
 admin.site.register(photos)
+admin.site.register(Category)
+admin.site.register(Location)
