6017bdecaf549af7f7d6fede21b80e051231a3d9
claudiahW <murungiclaud@gmail.com>
2021-12-01 19:44:17 +0300

    style navbar

--- tateke/settings.py
+++ tateke/settings.py
@@ -140,7 +140,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
