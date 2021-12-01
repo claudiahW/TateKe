3145f41d4ce0e6089e56aebee8e5274b46294b6e
claudiahW <murungiclaud@gmail.com>
2021-12-01 19:25:31 +0300

    add styling functions

--- gallery/models.py
+++ gallery/models.py
@@ -1,12 +1,29 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
+import datetime as dt
 
+class Category(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
+
+
+class Location(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
 
 class photos(models.Model):
      
     image= CloudinaryField('image')
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,null=True)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE,null=True)
+    post_date = models.DateTimeField(auto_now_add=True,null=True)
+
 
     def __str__(self):
         return self.title
@@ -17,6 +34,38 @@ class photos(models.Model):
     @classmethod
     def all_images(cls):
             pictures=cls.objects.all()
-            return pictures    
+            return pictures  
+    # update image
+    def update_photos(self, title, description, category,location):
+        self.title = title
+        self.description = description
+        self.location = location
+        self.category = category
+        self.save()
+
+    # get all images
+    @classmethod
+    def get_all_photos(cls):
+        today = dt.date.today()
+        images = photos.objects.all(post_date__date = today)
+        return images
+    
+    @classmethod
+    def search_by_category(cls,search_term):
+        app = cls.objects.filter(category_name_icontains=search_term)
+        return app
+
+    @classmethod
+    def filter_by_location(cls, location):
+        images = photos.objects.filter(location__name=location)
+        return images
+
+
+    @classmethod
+    def search(cls, search_term):
+        images_by_category = cls.search_by_category(search_term)
+        images_by_location = cls.filter_by_location(search_term)
+        return images_by_category.union(images_by_location)              
+
 
    
--- gallery/views.py
+++ gallery/views.py
@@ -1,12 +1,37 @@
+from django.http.response import Http404
 from django.shortcuts import render
 from django.http  import HttpResponse
-from .models import photos
-
+import datetime as dt
+from models import Category, Location, photos
+from django.core.exceptions import ObjectDoesNotExist
 
 
 def index(request):
-    photo = photos.objects.all()
-    return render(request,'all-art/index.html',{'photo':photo}) 
+    categories = Category.objects.all()
+    locations = Location.objects.all()
+    
+        
+    if 'location' in request.GET and request.GET["location"]:
+        location_id = request.GET.get("location")
+        photo = photos.objects.filter(location = location_id)
+       
+    else:
+       
+        photo = photos.objects.all()
+            
+    ctx = {'photo':photo, 'categories': categories, 'locations':locations }
+    return render(request, 'all-art/index.html',ctx)
 
+def search_results(request):
 
+    if 'photos' in request.GET and request.GET["photos"]:
+        search_term = request.GET.get("photos")
+        searched_photos = photos.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-art/search.html',{"message":message,"photos": searched_photos})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-art/search.html',{"message":message})   
 
